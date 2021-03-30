20913541aea24ac65f8a96d6569ccb61822ffd9a
Rumyana Dimitrieva <rumyanamm@gmail.com>
2021-03-30 16:46:58 -0700

    PreLoading fixed

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React, { useState, useEffect } from "react";
 import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
+import ClockLoader from "react-spinners/ClockLoader";
+
+import { ImSpoonKnife } from "react-icons/im";
 
 import Home from "./pages/Home";
 import RecipeInfoPage from "./pages/RecipeInfoPage";
@@ -7,7 +10,6 @@ import RecipesSearchPage from "./pages/RecipesSearchPage";
 import MealPlanPage from "./pages/MealPlanPage";
 import RecipesNutrients from "./pages/RecipesNutrientsPage";
 import SideBar from "./components/navigation/SideBar";
-import Preloader from "./components/Preloader";
 import "./App.css";
 
 const App = () => {
@@ -17,13 +19,36 @@ const App = () => {
     setLoading(true);
     setTimeout(() => {
       setLoading(false);
-    }, 1000);
+    }, 5000);
   }, []);
 
   return (
     <>
       {loading ? (
-        <Preloader />
+        <div className="loader-bg">
+          <div className="p-3 text-bold">
+            <span className="text-dark unsplash-text">
+              Photo by{" "}
+              <a href="https://unsplash.com/@ellaolsson?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">
+                Ella Olsson
+              </a>{" "}
+              on{" "}
+              <a href="https://unsplash.com/s/photos/cutting-board?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">
+                Unsplash
+              </a>
+            </span>
+          </div>
+          <div className="text-center text-md-right preloader-banner">
+            <div className="d-flex justify-content-center justify-content-md-end align-items-center mb-5 mr-0 pr-0 mr-md-5 pr-md-5">
+              <ClockLoader color="#ffaf2a" loading={loading} size={100} />
+            </div>
+            <p className="special-font preloader-font">
+              <ImSpoonKnife className="preloader-icon mr-3" />
+              Recipes
+            </p>
+            <h1 className="mb-5 preloader-text__2">your digital Cookbook</h1>
+          </div>
+        </div>
       ) : (
         <div className="main-container">
           <BrowserRouter>
